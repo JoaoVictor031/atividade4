f99a0d5e15f8ea08b45856e558cc55ee5536464b
Victor <101398802+JoaoVictor031@users.noreply.github.com>
2024-08-23 20:55:34 -0300

    Update AnimalMarinhoAB.java

--- src/AnimalMarinhoAB.java
+++ src/AnimalMarinhoAB.java
@@ -7,7 +7,7 @@ public abstract class AnimalMarinhoAB extends AnimalAB {
     @Override
     public abstract void comer();
     @Override
-    public abstract void moverse(); // Método nadar
+    public abstract void moverse();
     @Override
     public abstract void dormir();
 }
