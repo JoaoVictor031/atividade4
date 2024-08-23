5f7b2a029573666f9955f45b64037f399a1c931f
Victor <101398802+JoaoVictor031@users.noreply.github.com>
2024-08-23 20:55:57 -0300

    Update AnimalVoadorAB.java

--- src/AnimalVoadorAB.java
+++ src/AnimalVoadorAB.java
@@ -11,7 +11,7 @@ public abstract class AnimalVoadorAB extends AnimalAB {
     @Override
     public abstract void comer();
     @Override
-    public abstract void moverse(); // Método voar
+    public abstract void moverse();
     @Override
     public abstract void dormir();
 }
